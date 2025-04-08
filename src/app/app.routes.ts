a6021489ec57d1969796a6e618bc2946aaab339a
AkashTarmale97 <akashtarmale7@gmail.com>
2025-04-08 13:39:15 +0530

    First Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { ChangeRequestComponent } from './change-request/change-request.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        pathMatch:'full',
+        redirectTo:"index"
+
+    },
+    {
+        path: "change-request",
+        component:ChangeRequestComponent
+    },
+    {
+        path: "index",
+        component:MainPageComponent
+    }
+];
